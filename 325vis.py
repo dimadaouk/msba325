d6c2a595ed01588d1c2ef29025782b6e727d5305
dimadaouk <dmd10@mail.aub.edu>
2022-02-20 00:57:09 +0200

    ok

--- 325vis.py
+++ 325vis.py
@@ -203,13 +203,12 @@ else:
             st.plotly_chart(fig, use_container_width = True)
 
             # Comment about visualization
-            if st.button('Insight!'):
-                st.warning('''There seems to be an imbalance between salaries earned across different ethnic groups!
-                        We must work to end all forms of racial discrimination.''')
-                if st.button('Learn more!'):
-                    st.info('''To read more about laws forbidding racial discrimination in the workplace by the U.S. Equal Employment
-                            Opportunity Comission, check the following link: https://www.eeoc.gov/racecolor-discrimination''')
+            st.warning('''There seems to be an imbalance between salaries earned across different ethnic groups!
+                    We must work to end all forms of racial discrimination.''')
 
+            if st.button('Learn more!'):
+                st.info('''To read more about laws forbidding racial discrimination in the workplace by the U.S. Equal Employment
+                        Opportunity Comission, check the following link: https://www.eeoc.gov/racecolor-discrimination''')
 
         with col2:
             # Visualization 2: Barplot
